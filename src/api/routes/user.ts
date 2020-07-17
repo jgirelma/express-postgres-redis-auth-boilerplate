22ec9dd17acf3d7beb6ab3d54aa65caa3ea8a179
Jack <jgirelma@andrew.cmu.edu>
2020-07-17 17:10:15 -0400

    cookie deleted if session not found

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,15 +1,24 @@
 import { Router } from "express";
-import { loggedInMiddleware } from "../middleware/auth";
 import { getUserById } from "../controlers/userControler";
 
 const router = Router();
 
-router.post("/user", loggedInMiddleware, async (req, res) => {
-  const userId = req.session!.userId;
+const isLoggedIn = (req: any) => {
+  return !!req.session!.userId;
+};
 
-  const user = await getUserById(userId);
+router.post("/user", async (req, res) => {
+  if (!isLoggedIn(req)) {
+    res.clearCookie('sid')
+    res.status(401)
+    res.json({message: 'Not Authorized: Please Login'})
+  } else {
+    const userId = req.session!.userId;
 
-  res.status(200).json({ message: "OK", user });
+    const user = await getUserById(userId);
+
+    res.status(200).json({ message: "OK", user });
+  }
 });
 
-export {router as user}
+export { router as user };
