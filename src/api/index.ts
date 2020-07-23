ad9d002c8d1e900b72cb09b1dddbe14ace69aa6f
Jack <jgirelma@andrew.cmu.edu>
2020-07-22 22:35:40 -0400

    added new routes for posts etc

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,8 +3,17 @@ import { register } from "./routes/register"
 import { logout } from "./routes/logout"
 import { login } from "./routes/login"
 import { user } from "./routes/user"
+import { post } from "./routes/post"
+import { posts } from "./routes/posts"
+import { comments } from "./routes/comments"
+import { comment } from "./routes/comment"
 
 const router = Router()
+
+router.use((req, _res, next) => {
+  console.log(req)
+  next()
+})
 
 router.use(register)
 
@@ -13,5 +22,13 @@ router.use(logout)
 router.use(login)
 
 router.use(user)
+
+router.use(posts)
+
+router.use(post)
+
+router.use(comment)
+
+router.use(comments)
 
 export { router as apiRouter }
